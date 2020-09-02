d1afec963d2dfb0a89a6f529c78eab79dcb11f3d
sahandinuka1995 <59440188+sahandinuka1995@users.noreply.github.com>
2020-09-02 16:41:22 +0530

    Add files via upload

---
+++ src/animatefx/animation/FadeOut.java
@@ -0,0 +1,46 @@
+package animatefx.animation;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+/**
+ * @author Loïc Sculier aka typhon0
+ */
+public class FadeOut extends AnimationFX {
+    /**
+     * Create a new FadeOut animation
+     * @param node the node to affect
+     */
+    public FadeOut(Node node) {
+        super(node);
+    }
+
+    public FadeOut() {
+    }
+
+    public FadeOut(Stage stage) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    AnimationFX resetNode() {
+        getNode().setOpacity(1);
+        return this;
+    }
+
+    @Override
+    void initTimeline() {
+        setTimeline(new Timeline(
+                new KeyFrame(Duration.millis(0),
+                        new KeyValue(getNode().opacityProperty(), 1, AnimateFXInterpolator.EASE)
+                ),
+                new KeyFrame(Duration.millis(1000),
+                        new KeyValue(getNode().opacityProperty(), 0, AnimateFXInterpolator.EASE)
+                )
+        ));
+    }
+}
